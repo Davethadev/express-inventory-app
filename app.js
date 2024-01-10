955bc181331ddb3e6714f0e7a621e021e1284300
David Umanah <david_umanah@yahoo.com>
2024-01-10 15:04:29 +0100

    minor fix

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
@@ -13,10 +14,8 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const dev_db_url =
-  "mongodb+srv://david:WmNstTHKpgjkzG4K@cluster0.9hmmcek.mongodb.net/simple_inventory?retryWrites=true&w=majority";
 
-const mongoDB = process.env.PORT || dev_db_url;
+const mongoDB = process.env.PORT || process.env.MONGO_URI;
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
